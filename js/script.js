1229c49c34f18f2876669bf1e9fed8b4002c8cb0
ThushariSenevirathne <108646329+ThushariSenevirathne@users.noreply.github.com>
2024-12-08 13:02:57 +0530

    basic

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var hours = 6;
+
+// if (hours > 12){
+//     alert("Good Morning");
+// }else{
+//     alert("Good Afternoon");
+// }
+
+
+// if (hours <18){
+//     alert("Good morning");
+// }else if(hours>12){
+//     alert("Good afternoon");
+// }else{
+//     alert("Good Morning")
+// }
+
+(hours > 12) ? alert ("Good afternoon") : alert("Good morning")
---
+++ js/switch.js
@@ -0,0 +1,20 @@
+var dayOfWeek = 2;
+
+switch (dayOfWeek){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuseday");
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    default:
+        console.log("Invalid day of week");
+}
